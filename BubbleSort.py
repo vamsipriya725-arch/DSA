e6527998a44c79cad29ad943a8ca397d14e4f3fb
vamsipriya725-arch <vamsipriya725@gmail.com>
2026-08-14 12:14:54 +0530

    Implement bubble sort function
    Added a bubble_sort function to implement bubble sort algorithm.

--- BubbleSort.py
+++ BubbleSort.py
@@ -5,3 +5,14 @@ for b in range(len(a)):
             a[c], a[c + 1] = a[c + 1], a[c]
 
 print(a)
+
+#......With Function.........
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
